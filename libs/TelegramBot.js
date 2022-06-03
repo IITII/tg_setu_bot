df1f21f927efd05b4d41ad53297f454113c2f424
IITII <ccmejx@gmail.com>
2022-06-03 20:24:55 +0800

    fix: graceful stop

--- libs/TelegramBot.js
+++ libs/TelegramBot.js
@@ -8,6 +8,10 @@ const {BOT_TOKEN, PROXY} = require('../config/config')
 const HttpsProxyAgent = require('https-proxy-agent')
 
 module.exports = (() => {
+  if (!BOT_TOKEN) {
+    console.log('BOT_TOKEN is not defined')
+    process.exit(1)
+  }
   let bot = new Telegraf(BOT_TOKEN)
   if (PROXY) {
     const agent = new HttpsProxyAgent(PROXY)
