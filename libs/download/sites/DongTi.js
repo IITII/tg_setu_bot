3d85f072b9a0a56683ac3c0306069de2178c7592
IITII <ccmejx@gmail.com>
2022-07-06 01:52:56 +0800

    fix: undefined

--- libs/download/sites/DongTi.js
+++ libs/download/sites/DongTi.js
@@ -12,19 +12,19 @@ async function getImageArray(url) {
 
 async function handle_dom($, original) {
   const title = titleFormat($('.entry-head h1').text())
-  const imgsR = $('.entry-content figure img').map((i, el) => el.attribs.src).get(),
+  const imgsR = $('.entry-content img').map((i, el) => el.attribs['data-original']).get(),
     imgs = arrToAbsUrl(imgsR, original)
   const tagsR = $('.entry-tag a').map((i, el) => {
       const url = el.attribs.href
       const text = $(el).text()
       return {url, text}
-    }),
+    }).get(),
     tags = urlTextsToAbs(tagsR, original)
-  const relatedR = $('.entry-related-posts .entry-related a').map((i, el) => {
+  const relatedR = $('.entry-related-posts .entry-related .item-wrap').map((i, el) => {
       const url = el.attribs.href
       const text = el.attribs.title
       return {url, text}
-    }),
+    }).get(),
     related = urlTextsToAbs(relatedR, original)
   const res = {title, imgs, tags, related}
   return Promise.resolve(res)
