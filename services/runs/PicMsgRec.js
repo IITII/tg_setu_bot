43f9dfc0b9164221b29eef281b7408abb5acf2ff
IITII <ccmejx@gmail.com>
2022-08-29 13:45:31 +0800

    fix: send long text msg

--- services/runs/PicMsgRec.js
+++ services/runs/PicMsgRec.js
@@ -3,17 +3,17 @@
  * @date 2022/06/26
  */
 'use strict'
-const { queueName, eventName } = require('../../config/config'),
+const {queueName, eventName, telegram: tgLimit} = require('../../config/config'),
   eventBus = require('../../libs/event_bus'),
   Storage = require('../../libs/storage'),
-  { telegram } = require('../../libs/telegram_bot'),
+  {telegram} = require('../../libs/telegram_bot'),
   queue = queueName.pic_add,
   event = eventName.pic_add,
   storage = new Storage(queue)
-const { uniq } = require('lodash'),
-  { logger } = require('../../middlewares/logger'),
-  { message_decode } = require('../utils/service_utils'),
-  { send_text } = require('../utils/msg_utils'),
+const {uniq} = require('lodash'),
+  {logger} = require('../../middlewares/logger'),
+  {message_decode} = require('../utils/service_utils'),
+  {send_text} = require('../utils/msg_utils'),
   debMap = new Map()
 
 async function handle_ctx(ctx) {
@@ -21,21 +21,21 @@ async function handle_ctx(ctx) {
   const chat_id = message.chat.id,
     message_id = message.message_id,
     session = ctx.session
-  const { img_or_tags } = session.opts
+  const {img_or_tags} = session.opts
   const urls = message_decode(message, img_or_tags)
   if (urls.length === 0) {
     // const msg = `no url in message: ${JSON.stringify(message)}`
     const msg = `请检查当前模式,无法解析消息: ${JSON.stringify(message)}`
-    return ctx.reply(msg, { reply_to_message_id: message_id })
+    return ctx.reply(msg, {reply_to_message_id: message_id})
   }
-  const urlInfo = { chat_id, message_id, session, urls, }
+  const urlInfo = {chat_id, message_id, session, urls}
   return debounce(urlInfo)
 }
 
 // 按 message id 精细化区分
 async function debounce(urlInfo) {
-  const { chat_id, message_id, session, urls, } = urlInfo
-  let timerInfo = { timer: null, count: 0, urls: [], delay: 500 }
+  const {chat_id, message_id, session, urls} = urlInfo
+  let timerInfo = {timer: null, count: 0, urls: [], delay: 500}
   const mapKey = `${chat_id}_${JSON.stringify(session)}`
   if (debMap.has(mapKey)) {
     timerInfo = debMap.get(mapKey)
@@ -54,14 +54,19 @@ async function debounce(urlInfo) {
 
 async function timeout(chat_id, message_id, session, k, info) {
   const s = `#Add_Queue\n#${session.pic.mode}\n添加 ${info.count} 条链接到队列`
-  const v = { chat_id, message_id, session, urls: info.urls, }
+  const v = {chat_id, message_id, session, urls: info.urls}
   await storage.rpush(v)
   debMap.delete(k)
   eventBus.emit(event, v)
   try {
     const url_t = info.urls.join('\n')
-    // 消息插队
-    await telegram.sendMessage(chat_id, `${s}\n${url_t}`, { reply_to_message_id: message_id })
+    const msg = `${s}\n${url_t}`
+    if (msg >= tgLimit.maxMessageLength - 50) {
+      await send_text(chat_id, msg, message_id)
+    } else {
+      // 消息插队
+      await telegram.sendMessage(chat_id, msg, {reply_to_message_id: message_id, disable_web_page_preview: true})
+    }
   } catch (e) {
     logger.error(`${e.message}`)
     logger.error(e)
