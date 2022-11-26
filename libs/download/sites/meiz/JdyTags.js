2f128f3b2a33d07d2f9bcfc2a8d0af9e1db3e38b
IITII <ccmejx@gmail.com>
2022-11-26 17:59:30 +0800

    style: reformat

--- libs/download/sites/meiz/JdyTags.js
+++ libs/download/sites/meiz/JdyTags.js
@@ -3,48 +3,48 @@
  * @date 2022/11/03
  * @deprecated 网站响应太慢，放弃
  */
- 'use strict'
+'use strict'
 
- const {get_dom, getTagImgArr, urlTextsToAbs} = require('../../dl_utils')
- const jdy = require('./Jdy')
- const {titleFormat} = require('../../../utils')
- 
- module.exports = {
-   getTagUrls,
-   getImageArray,
- }
- 
- async function getTagUrls(url) {
-   return get_dom(url, handle_dom)
- }
- 
- async function getImageArray(url) {
-   return getTagImgArr(url, getTagUrls, jdy.getImageArray)
- }
- 
- async function handle_dom($, original) {
-   let title, posters, url_texts, urls, texts
- 
-   title = $('.collection-list-top h1').text() || $('.tax-title').text()
-   urls = $('.post-list .mobile-hidden a').map((i, el) => el.attribs.href).get()
-   url_texts = $('.post-list .mobile-hidden img').map((i, el) => {
-     const url = urls[i]
-     return {url, text: el.attribs.alt, poster: el.attribs['data-src']}
-   }).get()
-   // for tags
-   if (!(urls && urls.length > 0)) {
+const {get_dom, getTagImgArr, urlTextsToAbs} = require('../../dl_utils')
+const jdy = require('./Jdy')
+const {titleFormat} = require('../../../utils')
+
+module.exports = {
+  getTagUrls,
+  getImageArray,
+}
+
+async function getTagUrls(url) {
+  return get_dom(url, handle_dom)
+}
+
+async function getImageArray(url) {
+  return getTagImgArr(url, getTagUrls, jdy.getImageArray)
+}
+
+async function handle_dom($, original) {
+  let title, posters, url_texts, urls, texts
+
+  title = $('.collection-list-top h1').text() || $('.tax-title').text()
+  urls = $('.post-list .mobile-hidden a').map((i, el) => el.attribs.href).get()
+  url_texts = $('.post-list .mobile-hidden img').map((i, el) => {
+    const url = urls[i]
+    return {url, text: el.attribs.alt, poster: el.attribs['data-src']}
+  }).get()
+  // for tags
+  if (!(urls && urls.length > 0)) {
     urls = $('.post_tag .post-module-thumb a').map((i, el) => el.attribs.href).get()
-   }
-   if (!(url_texts && url_texts.length > 0)) {
+  }
+  if (!(url_texts && url_texts.length > 0)) {
     url_texts = $('.post-list .post-module-thumb img').map((i, el) => {
       const url = urls[i]
       return {url, text: el.attribs.alt, poster: el.attribs['data-src']}
     }).get()
-   }
- 
-   title = titleFormat(title)
-   url_texts = urlTextsToAbs(url_texts, original, true)
-   const res = {title, imgs: url_texts}
-   return Promise.resolve(res)
- }
+  }
+
+  title = titleFormat(title)
+  url_texts = urlTextsToAbs(url_texts, original, true)
+  const res = {title, imgs: url_texts}
+  return Promise.resolve(res)
+}
 
--- libs/download/sites/meiz/MuWeiTags.js
+++ libs/download/sites/meiz/MuWeiTags.js
@@ -4,60 +4,54 @@
  */
 'use strict'
 
-/**
- * @author IITII <ccmejx@gmail.com>
- * @date 2022/11/03
- */
- 'use strict'
+const {get_dom, getTagImgArr, urlTextsToAbs} = require('../../dl_utils')
+const {titleFormat} = require('../../../utils')
+const muWei = require('./MuWei')
 
- const {get_dom, getTagImgArr, urlTextsToAbs} = require('../../dl_utils')
- const {titleFormat} = require('../../../utils')
- const muWei = require('./MuWei')
- 
- module.exports = {
-   getTagUrls,
-   getImageArray,
- }
- 
- async function getTagUrls(url) {
-   return get_dom(url, handle_dom)
- }
- 
- async function getImageArray(url) {
-   return getTagImgArr(url, getTagUrls, muWei.getImageArray)
- }
- 
- async function handle_dom($, original) {
-   let title, posters, url_texts, urls, texts
- 
-   title = $('#content-container .feature-header h1').text()
-   posters = $('#main-wrap .item-feature').map((i,el) => {
+module.exports = {
+  getTagUrls,
+  getImageArray,
+}
+
+async function getTagUrls(url) {
+  return get_dom(url, handle_dom)
+}
+
+async function getImageArray(url) {
+  return getTagImgArr(url, getTagUrls, muWei.getImageArray)
+}
+
+async function handle_dom($, original) {
+  let title, posters, url_texts, urls, texts
+
+  title = $('#content-container .feature-header h1').text()
+  posters = $('#main-wrap .item-feature').map((i, el) => {
     let bg, key = 'background-image:'
     bg = el.attribs['style']
     bg = bg.split(/\s*;\s*/).filter(_ => _.includes(''))[0]
     bg = bg.replace(key, '').trim()
     if (bg.startsWith('url')) {
-        bg = bg.replace(/(url|[\(\)"'])/g, '')
+      bg = bg.replace(/(url|[\(\)"'])/g, '')
     }
     return bg
-   }).get()
-   url_texts = $('#main-wrap .item-title a').map((i, el) => {
-     const e = posters[i]
-     return {url: el?.attribs?.href, text: el?.attribs.title, poster: e}
-   }).get()
-   // for tags
-   if (!(urls && urls.length > 0)) {
+  }).get()
+  url_texts = $('#main-wrap .item-title a').map((i, el) => {
+    const e = posters[i]
+    return {url: el?.attribs?.href, text: el?.attribs.title, poster: e}
+  }).get()
+  // for tags
+  if (!(urls && urls.length > 0)) {
     urls = $('.post_tag .post-module-thumb a').map((i, el) => el.attribs.href).get()
-   }
-   if (!(url_texts && url_texts.length > 0)) {
+  }
+  if (!(url_texts && url_texts.length > 0)) {
     url_texts = $('.post-list .post-module-thumb img').map((i, el) => {
       const url = urls[i]
       return {url, text: el.attribs.alt, poster: el.attribs['data-src']}
     }).get()
-   }
- 
-   title = titleFormat(title)
-   url_texts = urlTextsToAbs(url_texts, original, true)
-   const res = {title, imgs: url_texts}
-   return Promise.resolve(res)
- }
+  }
+
+  title = titleFormat(title)
+  url_texts = urlTextsToAbs(url_texts, original, true)
+  const res = {title, imgs: url_texts}
+  return Promise.resolve(res)
+}
